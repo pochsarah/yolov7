81af7d610b82ebae01a63bc415c48248f8715db8
PochS <sarah.poch@hotmail.fr>
2023-05-02 11:34:04 +0200

    Update 02.05

--- thesis.sh
+++ thesis.sh
@@ -9,25 +9,9 @@ setcuda 11.7
 conda activate yolov7
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/share/castor/home/e1800093/.conda/envs/yolov7/lib
 
-#python -u train.py --img-size 416 --hyp data/hyp.scratch.custom.yaml --cfg cfg/training/yolov7.yaml --data data/subset_8.yaml --weights yolov7_training.pt --workers 4 --project v4/train --name baseline_sub_8 --device 0 --single-cls --nosave
-#python -u train.py --img-size 416 --hyp data/hyp.scratch.custom.yaml --cfg cfg/training/yolov7.yaml --data data/subset_9.yaml --weights yolov7_training.pt --workers 4 --project v4/train --name baseline_sub_9 --device 0 --single-cls --nosave
-#python -u train.py --img-size 416 --hyp data/hyp.scratch.custom.yaml --cfg cfg/training/yolov7.yaml --data data/subset_10.yaml --weights yolov7_training.pt --workers 4 --project v4/train --name baseline_sub_10 --device 0 --single-cls --nosave
-#python -u train.py --img-size 416 --epochs 100 --hyp data/hyp.scratch.custom.yaml --cfg cfg/training/yolov7.yaml --data data/subset_1.yaml --weights yolov7_training.pt --workers 4 --project v4/train --name baseline_sub_1_evolve --device 0 --single-cls --nosave --evolve --cache-images
-#rm evolve.txt 
-#python -u train.py --img-size 416 --epochs 100 --hyp data/hyp.scratch.custom.yaml --cfg cfg/training/yolov7.yaml --data data/subset_2.yaml --weights yolov7_training.pt --workers 4 --project v4/train --name baseline_sub_2_evolve --device 0 --single-cls --nosave --evolve --cache-images
-#rm evolve.txt 
-#python -u train.py --img-size 416 --epochs 100 --hyp data/hyp.scratch.custom.yaml --cfg cfg/training/yolov7.yaml --data data/subset_3.yaml --weights yolov7_training.pt --workers 4 --project v4/train --name baseline_sub_3_evolve --device 0 --single-cls --nosave --evolve --cache-images
-#rm evolve.txt 
-#python -u train.py --img-size 416 --epochs 100 --hyp data/hyp.scratch.custom.yaml --cfg cfg/training/yolov7.yaml --data data/subset_4.yaml --weights yolov7_training.pt --workers 4 --project v4/train --name baseline_sub_4_evolve --device 0 --single-cls --nosave --evolve --cache-images
-#rm evolve.txt 
-#python -u train.py --img-size 416 --epochs 100 --hyp data/hyp.scratch.custom.yaml --cfg cfg/training/yolov7.yaml --data data/subset_5.yaml --weights yolov7_training.pt --workers 4 --project v4/train --name baseline_sub_5_evolve --device 0 --single-cls --nosave --evolve --cache-images
-#rm evolve.txt 
-#python -u train.py --img-size 416 --epochs 100 --hyp data/hyp.scratch.custom.yaml --cfg cfg/training/yolov7.yaml --data data/subset_6.yaml --weights yolov7_training.pt --workers 4 --project v4/train --name baseline_sub_6_evolve --device 0 --single-cls --nosave --evolve --cache-images
-#rm evolve.txt 
-#python -u train.py --img-size 416 --epochs 100 --hyp data/hyp.scratch.custom.yaml --cfg cfg/training/yolov7.yaml --data data/subset_7.yaml --weights yolov7_training.pt --workers 4 --project v4/train --name baseline_sub_7_evolve --device 0 --single-cls --nosave --evolve --cache-images
-#rm evolve.txt 
-#python -u train.py --img-size 416 --epochs 100 --hyp data/hyp.scratch.custom.yaml --cfg cfg/training/yolov7.yaml --data data/subset_8.yaml --weights yolov7_training.pt --workers 4 --project v4/train --name baseline_sub_8_evolve --device 0 --single-cls --nosave --evolve --cache-images
-#rm evolve.txt 
-python -u train.py --img-size 416 --epochs 100 --hyp data/hyp.scratch.custom.yaml --cfg cfg/training/yolov7.yaml --data data/subset_9.yaml --weights yolov7_training.pt --workers 4 --project v4/train --name baseline_sub_9_evolve --device 0 --single-cls --nosave --evolve --cache-images
-rm evolve.txt 
-python -u train.py --img-size 416 --epochs 100 --hyp data/hyp.scratch.custom.yaml --cfg cfg/training/yolov7.yaml --data data/subset_10.yaml --weights yolov7_training.pt --workers 4 --project v4/train --name baseline_sub_10_evolve --device 0 --single-cls --nosave --evolve --cache-images
+python -u train.py --weights yolov7_training.pt --cfg cfg/training/yolov7.yaml --data data/subset_1.yaml --hyp data/hyp.scratch.custom.yaml --img-size 416 --device 0 --single-cls --workers 4 --project v5/train --name final_baseline_subset1_custom_linear_SGD
+python -u test.py --weights v5/train/final_baseline_subset1_custom_linear_SGD/weights/best.pt --data data/subset_1.yaml --img-size 416 --task test --device 0 --single-cls --save-txt --save-conf --project v5/test --name final_baseline_subset1_custom_linear_SGD
+rm -r data_cluster/baseline/subset_1/*.cache
+python -u train.py --weights yolov7_training.pt --cfg cfg/training/yolov7.yaml --data data/subset_1.yaml --hyp data/hyp.scratch.evolved.yaml --img-size 416 --device 0 --single-cls --workers 4 --project v5/train --name final_baseline_subset1_evolved_linear_SGD
+python -u test.py --weights v5/train/final_baseline_subset1_evolved_linear_SGD/weights/best.pt --data data/subset_1.yaml --img-size 416 --task test --device 0 --single-cls --save-txt --save-conf --project v5/test --name final_baseline_subset1_evolved_linear_SGD
+rm -r data_cluster/baseline/subset_1/*.cache
