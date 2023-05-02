bc58d8ce967c53fec31fc43087e9dbd00814d521
Poch Sarah <e1800093@cluster-irisa.univ-ubs.fr>
2023-05-02 13:30:01 +0200

    Update 02.05

--- thesis.sh
+++ thesis.sh
@@ -9,9 +9,9 @@ setcuda 11.7
 conda activate yolov7
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/share/castor/home/e1800093/.conda/envs/yolov7/lib
 
-python -u train.py --weights yolov7_training.pt --cfg cfg/training/yolov7.yaml --data data/subset_1.yaml --hyp data/hyp.scratch.custom.yaml --img-size 416 --device 0 --single-cls --workers 4 --project v5/train --name final_baseline_subset1_custom_linear_SGD
+python -u train.py --weights yolov7_training.pt --cfg cfg/training/yolov7.yaml --data data/subset_1.yaml --hyp data/hyp.scratch.custom.yaml --img-size 416 --device 0 --single-cls --workers 4 --project v5/train --name final_baseline_subset1_custom_linear_SGD --linear-lr
 python -u test.py --weights v5/train/final_baseline_subset1_custom_linear_SGD/weights/best.pt --data data/subset_1.yaml --img-size 416 --task test --device 0 --single-cls --save-txt --save-conf --project v5/test --name final_baseline_subset1_custom_linear_SGD
 rm -r data_cluster/baseline/subset_1/*.cache
-python -u train.py --weights yolov7_training.pt --cfg cfg/training/yolov7.yaml --data data/subset_1.yaml --hyp data/hyp.scratch.evolved.yaml --img-size 416 --device 0 --single-cls --workers 4 --project v5/train --name final_baseline_subset1_evolved_linear_SGD
+python -u train.py --weights yolov7_training.pt --cfg cfg/training/yolov7.yaml --data data/subset_1.yaml --hyp data/hyp.scratch.evolved.yaml --img-size 416 --device 0 --single-cls --workers 4 --project v5/train --name final_baseline_subset1_evolved_linear_SGD --linear-lr
 python -u test.py --weights v5/train/final_baseline_subset1_evolved_linear_SGD/weights/best.pt --data data/subset_1.yaml --img-size 416 --task test --device 0 --single-cls --save-txt --save-conf --project v5/test --name final_baseline_subset1_evolved_linear_SGD
 rm -r data_cluster/baseline/subset_1/*.cache
