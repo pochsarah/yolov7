5dbc2146841abc97e1fec457e51a6e84f0eb0429
PochS <sarah.poch@hotmail.fr>
2023-05-25 12:11:29 +0200

    Update thesis.sh

--- thesis.sh
+++ thesis.sh
@@ -9,25 +9,7 @@ setcuda 11.7
 conda activate yolov7
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/share/castor/home/e1800093/.conda/envs/yolov7/lib
 
-#custom - linear - sgd
-#python -u train.py --weights yolov7_training.pt --cfg cfg/training/yolov7.yaml --data data/subset_1.yaml --hyp data/hyp.scratch.custom.yaml --img-size 416 --device 0 --single-cls --workers 4 --project v5/train --name final_baseline_subset1_custom_linear_SGD --linear-lr
-#python -u test.py --weights v5/train/final_baseline_subset1_custom_linear_SGD/weights/best.pt --data data/subset_1.yaml --img-size 416 --task test --device 0 --single-cls --save-txt --save-conf --project v5/test --name final_baseline_subset1_custom_linear_SGD
-#rm -r data_cluster/baseline/subset_1/*.cache
-#evolved - linear -sgd
-#python -u train.py --weights yolov7_training.pt --cfg cfg/training/yolov7.yaml --data data/subset_1.yaml --hyp data/hyp.scratch.evolved.yaml --img-size 416 --device 0 --single-cls --workers 4 --project v5/train --name final_baseline_subset1_evolved_linear_SGD --linear-lr
-#python -u test.py --weights v5/train/final_baseline_subset1_evolved_linear_SGD/weights/best.pt --data data/subset_1.yaml --img-size 416 --task test --device 0 --single-cls --save-txt --save-conf --project v5/test --name final_baseline_subset1_evolved_linear_SGD
-#rm -r data_cluster/baseline/subset_1/*.cache
 
-#evolved - one cycle -sgd
-#python -u train.py --weights yolov7_training.pt --cfg cfg/training/yolov7.yaml --data data/subset_1.yaml --hyp data/hyp.scratch.evolved.yaml --img-size 416 --device 0 --single-cls --workers 4 --project v5/train --name final_baseline_subset1_evolved_onecycle_SGD
-#rm -r data_cluster/baseline/subset_1/*.cache
-#evolved - linear - adam
-#python -u train.py --weights yolov7_training.pt --cfg cfg/training/yolov7.yaml --data data/subset_1.yaml --hyp data/hyp.scratch.evolved.yaml --img-size 416 --device 0 --single-cls --workers 4 --project v5/train --name final_baseline_subset1_evolved_linear_Adam --linear-lr --adam
-#rm -r data_cluster/baseline/subset_1/*.cache
+#  --name test_layers --linear-lr
 
-#python train_al.py --img-size 416 --epochs 12 --hyp data/hyp.scratch.evolved.yaml --cfg cfg/training/yolov7.yaml --data data/subset_test_al.yaml --weights yolov7_training.pt --workers 4 --project v7/train  --name al_v0 --device 0 --single-cls 
-#--project-test v7/test --task test --save-txt
-
-#python train_active.py --img-size 416 --epochs 15 --hyp data/hyp.scratch.evolved.yaml --cfg cfg/training/yolov7.yaml --data data/subset_test_al.yaml --weights yolov7_training.pt --workers 4 --project v8/train --name al --device 0 --single-cls
-
-python -u train.py --weights yolov7_training.pt --cfg cfg/training/yolov7_custom.yaml --data data/subset_1.yaml --hyp data/hyp.scratch.evolved.yaml --img-size 416 --device 0 --single-cls --workers 4 --project v9_layer --name test_layers --linear-lr
+python -u train_loop.py --weights yolov7_training.pt --cfg cfg/training/yolov7.yaml --data data/subset_test_al.yaml --hyp data/hyp.scratch.evolved.yaml --epochs 12 --img-size 416 --device 0 --single-cls --workers 4 --project act_lear --name run1 --linear-lr --save-txt --save-hybrid
