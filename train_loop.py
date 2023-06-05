591cf6a43a83a07dc7842aafcf398548f932866f
Poch Sarah <sarah.poch@hotmail.fr>
2023-06-05 08:24:48 +0200

    Updates 05.06

--- train_loop.py
+++ train_loop.py
@@ -752,7 +752,7 @@ if __name__ == '__main__':
         path_label = path_label.replace('train', 'test')
         path_label += "/labels/"
         
-        method = "maximum"
+        method = "random"
         
         if method == "random":
             j = draft.random_chunk(path_data, chunks)
